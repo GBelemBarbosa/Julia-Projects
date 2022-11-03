373bd3331a1ef4e1007b49c135104dc118f90c50
GBelemBarbosa <107588056+GBelemBarbosa@users.noreply.github.com>
2022-11-03 19:26:20 -0300

    embellishment + quasi-colinear

--- MS993/project_almost.jl
+++ MS993/project_almost.jl
@@ -10,7 +10,10 @@ ylim=f.(xlim)
 m=8
 x₁=2 .-4 .*rand(m)
 x₂=f.(x₁)
+ϵ=-0.2
+x₂[end]-=1
 y=g.(x₁)
+y[end]+=ϵ
 X=hcat(ones(m, 1), x₁, x₂)
 β=X'*X\(X'*y)
 plt=plot3d(xlim, ylim, vec(zeros(2, 1)),
@@ -19,17 +22,22 @@ plt=plot3d(xlim, ylim, vec(zeros(2, 1)),
     ylabel=L"x_2",
     zlabel=L"y",
     zlims=(0, 4.5),
-    title="Dados colineares",
+    title="Dados quasi-colineares",
     labels="")
 plot3d!(xlim, ylim, hcat(ones(2, 1), xlim, ylim)*β, labels="Previsão")
-scatter!(x₁, x₂, vec(zeros(m, 1)), labels="", color="green")
-scatter!(x₁, x₂, y, labels="Dados", color="pink")
-for i=eachindex(x₁)
-    plot3d!([x₁[i], x₁[i]], [x₂[i], x₂[i]], [0, y[i]], line=(:dash), color="orange", labels="")
+scatter!(plt, x₁[1:m-1], x₂[1:m-1], vec(zeros(m-1, 1)), labels="", color="green")
+scatter!(plt, [x₁[end]], [x₂[end]], [0], labels="", color="green")
+for i=1:m-1
+    plot3d!(plt, [x₁[i], x₁[i]], [x₂[i], x₂[i]], [0, y[i]], line=(:dash), color="orange", labels="")
 end
+scatter!(plt, x₁[1:m-1], x₂[1:m-1], y[1:m-1], labels="Dados colineares", color="pink")
+plt0=deepcopy(plt)
+scatter!(plt, [x₁[end]], [x₂[end]], [y[end]], labels="Dado não colinear", color="red")
+plot3d!(plt, [x₁[end], x₁[end]], [x₂[end], x₂[end]], [0, y[end]], line=(:dash), color="orange", labels="")
 display(plt)
 sleep(10)
 
+scatter!(plt0, [-1], [10], [0], labels="Dado a prever", color="darkblue")
 plt1=deepcopy(plt)
 scatter!(plt1, [-1], [10], [0], labels="Dado a prever", color="darkblue")
 display(plt1)
@@ -49,13 +57,15 @@ Vt=V'
 U, Σ, Vt=U[:, 1:2], Σ[1:2], Vt[1:2, :]
 α=U'*y
 Z=Diagonal(Σ)*Vt
-c=1
-anim = @animate for i=0:0.01:c
-    plt3=deepcopy(plt1)
-    β[3]=abs(i-c/2)
-    β[1], β[2]=Z[:, 1:2]\(α-Z[:, 3]*β[3])
+anim = @animate for i=0:0.05:2
+    plt3=deepcopy(plt0)
+    y[end]-=0.05*ϵ
+    β=X'*X\(X'*y)
+    plane(x, y)=β[1]+x*β[2]+y*β[3]
+    scatter!(plt3, [x₁[end]], [X[end, end]], [y[end]], labels="Dado não colinear mais erro", color="red")
+    plot3d!(plt3, [x₁[end], x₁[end]], [x₂[end], x₂[end]], [0, y[end]], line=(:dash), color="orange", labels="")
     surface!(plt3, -2:0.1:2, 6:0.1:18, plane, alpha=0.4, colorbar=false)
     scatter!(plt3, [-1], [10], [plane(-1, 10)], labels="Previsão do novo dado", color="magenta")
     plot3d!(plt3, [-1, -1], [10, 10], [0, plane(-1, 10)], line=(:dash), color="orange", labels="")
 end
-gif=gif(anim, "plane.gif", fps=10)
+gif=gif(anim, "plane_almost.gif", fps=10)
